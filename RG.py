54e6b732f0812263ba8301d5c6cf5f01e40d9cd1
gutessitore <gugatessi@gmail.com>
2019-11-21 23:00:37 -0300

    Update

--- RG.py
+++ RG.py
@@ -1,6 +1,7 @@
-from requests import get
-from extract import extract
 from bs4 import BeautifulSoup
+from extract import extract
+from requests import get
+from tqdm import tqdm
 import json
 import art
 
@@ -10,10 +11,37 @@ print('\033[7m' + "by GuTessitore" + '\033[0m')
 print("‾"*30)
 year = input("enter a year: ")
 
+output = '''
+<!DOCTYPE html>
+<html>
+<head>
+<title>Reference generator</title>
+</head>
+<body style="background-color: #f8f8f8;
+margin: 0;
+padding: 0;
+border: 0;">
+
+<div style="background-color: #e7e7e7;
+width: 100%;
+height: 3em;">
+</div>
+
+<div style="margin-left: auto;
+margin-right: auto;
+width: 18em;
+font-size: 2em;">
+<h1 style="margin: 0; ">Reference Generator</h1>
+</div>
+
+<div style="padding: 10px;">
+<div>
+'''
+
 with open("links.json") as links:
 	urls = json.load(links)
 
-for url in urls["links"]:
+for url in tqdm(urls["links"]):
 
 	try:
 		html = get(url).content
@@ -26,10 +54,22 @@ for url in urls["links"]:
 
 		reference = extract.Reference(titulo, nome, url, data)
 
-		print(reference)
-		print("\n")
+		# print(reference)
+		# print("\n")
+
+		output += "<p>" + reference + "</p>"
 
 	except:
-		print('\033[31m' + "error: page not found <", url, ">" + '\033[0m')
+		print('\033[31m' + "error: page not found <" + url + ">" + '\033[0m' + "\n")
 
+output += '''
+</div>
+</div>
 
+</body>
+</html>
+'''
+
+html_file = open("page.html", "w")
+html_file.write(output)
+html_file.close()
--- setup.py
+++ setup.py
@@ -1,30 +1,54 @@
+import platform
 import os
+
+running_os = platform.system()
+
+try:
+	import pip
+except:
+
+	if running_os == "Linux":
+		install = input("Do you want to install pip? [y/n]: ")
+		if install == "y":
+			os.system('sudo apt update')
+			os.system('sudo apt install python3-pip')
+		else:
+			exit()
+
+	elif running_os == "Windows":
+		pass
+
+	elif running_os == "Darwin":
+		pass
+
+	else:
+		print('\033[31m' + "Can't find operating system" + '\033[0m')
 
 try:
 	import bs4
 except:
-	print("Trying to Install required module: bs4\n")
+	print('\033[33m' + "Trying to Install required module: bs4\n" + '\033[0m')
 	os.system('python -m pip install bs4')
 
 
 try:
 	import requests
 except:
-	print("Trying to Install required module: requests\n")
+	print('\033[33m' + "Trying to Install required module: requests\n" + '\033[0m')
 	os.system('python -m pip install requests')
 
 
 try:
 	import json
 except:
-	print("Trying to Install required module: json\n")
+	print('\033[33m' + "Trying to Install required module: json\n" + '\033[0m')
 	os.system('python -m pip install json')
 
 
 try:
 	import art
 except:
-	print("Trying to Install required module: art\n")
+	print('\033[33m' + "Trying to Install required module: art\n" + '\033[0m')
 	os.system('python -m pip install art')
 
 
@@ -32,18 +56,24 @@ except:
 try:
 	import datetime
 except:
-  	print("Trying to Install required module: datetime\n")
+  	print('\033[33m' + "Trying to Install required module: datetime\n" + '\033[0m')
   	os.system('python -m pip install datetime')
 
 
 try:
 	import random
 except:
-  	print("Trying to Install required module: random\n")
+  	print('\033[33m' + "Trying to Install required module: random\n" + '\033[0m')
   	os.system('python -m pip install random')
 
+try:
+	import tqdm
+except:
+  	print('\033[33m' + "Trying to Install required module: tqdm\n" + '\033[0m')
+  	os.system('python -m pip install tqdm')
+
 print('\033[32m' + "All modules installed." + '\033[0m')
-print('\033[7m' + "Don't forget to setup 'links.json' file to have all links that will be referenced" + '\033[0m')
+print('\033[33m' + "Don't forget to setup 'links.json' file to have all links that will be referenced" + '\033[0m')
 
 
 
